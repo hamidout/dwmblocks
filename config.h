45ce2cd555ef26fca34f37f6a009933f6b8c3ae2
hamid <hamid@hamidout.xyz>
2021-01-05 13:14:12 +0100

    scripts prefixed with sb- to avoid conflicts

--- config.h
+++ config.h
@@ -1,22 +1,22 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"⌨", "kbselect", 0, 30},
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"music",	0,	11},
-	{"",	"corona",	18000,	20},
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
-	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	{"",	"cpu",		10,	18},
-	/* {"",	"forecast",	18000,	5}, */
-	{"",	"mailbox",	180,	12},
-	{"",	"nettraf",	1,	16},
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
+	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
+	{"⌨",		"sb-kbselect", 0, 30},
+	{"",		"cat /tmp/recordingicon 2>/dev/null",	0,			9},
+	{"",		"sb-music",				0,			11},
+	{"",		"sb-corona",				18000,			20},
+	{"",		"sb-pacpackages",			0,			8},
+	{"",		"sb-news",				0,			6},
+	/* {"",		"sb-torrent",				20,			7}, */
+	/* {"",		"sb-memory",				10,			14}, */
+	{"",		"sb-cpu",				10,			18},
+	/* {"",		"sb-forecast",				18000,			5}, */
+	{"",		"sb-mailbox",				180,			12},
+	{"",		"sb-nettraf",				1,			16},
+	{"",		"sb-volume",				0,			10},
+	{"",		"sb-battery",				5,			3},
+	{"",		"sb-clock",				60,			1},
+	{"",		"sb-internet",				5,			4},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
